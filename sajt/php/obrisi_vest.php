b65463b3171f526659f89025b1b7c4497309bc7b
Luka Bogdanovic <125151973+LukaBogdanovic@users.noreply.github.com>
2023-04-05 10:26:59 +0200

    Update obrisi_vest.php

--- sajt/php/obrisi_vest.php
+++ sajt/php/obrisi_vest.php
@@ -1,10 +1,10 @@
 <?php
 include "bazapodatakacon.php";
-include " klase.php"
+include "klase.php"
 if (isset($_GET['id'])) {
 	$id = $_GET['id'];
-	$direktor= new Direktor();
-	 $direktor->obrisi_vest($id);
+	$vest= new Vest();
+	 $vest->obrisi_vest($id);
 }
 header("Location: prikaz.php")
 ?>
